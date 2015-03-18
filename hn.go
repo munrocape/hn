f1aff190cafe74ec71c959f00358973cdd4e2b8f
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-18 20:53:17 -0300

    Show, Ask, Job stories

--- hn.go
+++ hn.go
@@ -13,8 +13,10 @@ type Client struct {
 	ItemSuffix string
 	MaxSuffix string
 	TopSuffix string
+	NewSuffix string
 	JobSuffix string
 	AskSuffix string
+	ShowSuffix string
 	UpdateSuffix string
 }
 
@@ -28,6 +30,7 @@ func NewClient() *Client {
 	c.NewSuffix = "newstories.json"
 	c.JobSuffix = "jobstories.json"
 	c.AskSuffix = "askstories.json"
+	c.ShowSuffix = "showstories.json"
 	c.UpdateSuffix = "updates.json"
 	return &c
 }
@@ -74,7 +77,8 @@ func (c *Client) GetUser(username string) (User, error) {
 	return user, err
 }
 
-// GetTopStories takes an int number and returns an array of up to number ints that represent the top stories.
+// GetTopStories takes an int number and returns an array of up to number ints that represent the current top stories.
+// Constraints: 0 <= number <= 500
 func (c *Client) GetTopStories(number int) ([]int, error) {
 	var top500 []int
 	if number > 500 {
@@ -94,6 +98,7 @@ func (c *Client) GetTopStories(number int) ([]int, error) {
 }
 
 // GetNewStories takes an int number and returns an array of up to number ints that represent the newest stories.
+// Constraints: 0 <= number <= 500
 func (c *Client) GetNewStories(number int) ([]int, error) {
 	var top500 []int
 	if number > 500 {
@@ -112,6 +117,66 @@ func (c *Client) GetNewStories(number int) ([]int, error) {
 	return top500[:number], nil
 }
 
+// GetRecentAskStories takes an int number and returns an array of up to number ints that represent the most recent Ask stories
+// Constraints: 0 <= number <= 200
+func (c *Client) GetRecentAskStories(number int) ([]int, error) {
+	var top200 []int
+	if number > 200 {
+		return top200, fmt.Errorf("Number %d greater than maximum 500 items allowed", number)
+	}
+	
+	url := c.BaseUrl + c.AskSuffix
+	rep, err := c.GetResource(url)
+
+	err = json.Unmarshal(rep, &top200)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return top200[:number], nil
+}
+
+// GetRecentShowStories takes an int number and returns an array of up to number ints that represent the most recent Show stories
+// Constraints: 0 <= number <= 200
+func (c *Client) GetRecentShowStories(number int) ([]int, error) {
+	var top200 []int
+	if number > 200 {
+		return top200, fmt.Errorf("Number %d greater than maximum 500 items allowed", number)
+	}
+	
+	url := c.BaseUrl + c.ShowSuffix
+	rep, err := c.GetResource(url)
+
+	err = json.Unmarshal(rep, &top200)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return top200[:number], nil
+}
+
+// GetRecentJobStories takes an int number and returns an array of up to number ints that represent the most recent Job stories
+// Constraints: 0 <= number <= 200
+func (c *Client) GetRecentJobStories(number int) ([]int, error) {
+	var top200 []int
+	if number > 200 {
+		return top200, fmt.Errorf("Number %d greater than maximum 500 items allowed", number)
+	}
+	
+	url := c.BaseUrl + c.JobSuffix
+	rep, err := c.GetResource(url)
+
+	err = json.Unmarshal(rep, &top200)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return top200[:number], nil
+}
+
 func main() {
 	c := NewClient()
 
@@ -124,6 +189,18 @@ func main() {
 	comment, _ := c.GetItem(8715677)
 	fmt.Printf("%+v\n\n", comment)
 
-	top10, _ := c.GetTop(10)
+	top10, _ := c.GetTopStories(10)
 	fmt.Printf("%+v\n\n", top10)
+
+	new10, _ := c.GetNewStories(10)
+	fmt.Printf("%+v\n\n", new10)
+
+	ask10, _ := c.GetRecentAskStories(10)
+	fmt.Printf("%+v\n\n", ask10)
+
+	job10, _ := c.GetRecentJobStories(10)
+	fmt.Printf("%+v\n\n", job10)
+
+	show10, _ := c.GetRecentShowStories(10)
+	fmt.Printf("%+v\n\n", show10)
 }
