a4711611c2ad300e9ffdd6f93da6a37c5ab0d467
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-19 18:01:01 -0300

    Moved license

--- client/story.go
+++ client/story.go
@@ -3,10 +3,10 @@ package client
 type Story struct {
 	By          string `json:"by"`
 	Descendants int    `json:"descendants"`
+	Id          int    `json:"id"`
 	Kids        []int  `json:"kids"`
 	Score       int    `json:"score"`
 	Time        int    `json:"time"`
 	Title       string `json:"title"`
-	Type        string `json:"type"`
 	Url         string `json:"url"`
 }
